2580b1f52a2e6ba799874e932fe30e0505a731f4
Xavier Dutoit <civicrm@tttp.eu>
2012-08-02 02:41:45 +0200

    initial commit

---
+++ civicrm.js
@@ -0,0 +1,47 @@
+var request = require('request');
+var querystring = require('querystring');
+
+/*
+ex:
+var config = {
+  server:'http://example.org',
+  path:'/sites/all/modules/civicrm/extern/rest.php',
+  key:'your key from settings.civicrm.php',
+  api_key:'the user key'
+};
+*/
+var crmAPI = function () {
+  this.options ={};
+};
+
+
+crmAPI.init = function (options) {
+  this.options = options;
+};
+
+crmAPI.urlize = function (entity,action) {
+  return this.options.server +this.options.path+ '?' + querystring.stringify ({ 
+    entity: entity,
+    action: action,
+  });
+}
+
+crmAPI.call = function (entity,action,params,callback) {
+  var post = params;
+  post.sequential= 1;
+  post.json = 1;
+  post.key = this.options.key;
+  post.api_key = this.options.api_key;
+  request({
+    uri:this.urlize(entity,action),method:'POST',
+    form: post}
+    ,function(error, response, body){
+       if (!error && response.statusCode == 200) {
+        callback (JSON.parse(body));
+       } else {
+         callback ({is_error:1, error_message: 'invalid url'+uri});
+       }
+  });
+};
+
+module.exports = crmAPI;
