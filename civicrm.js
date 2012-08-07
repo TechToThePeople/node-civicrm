e3da5a37b5fa61caafe6b0ac8b20d66ac9c54f28
Xavier Dutoit <civicrm@tttp.eu>
2012-08-07 10:47:55 +0200

    can access more than one server at the same time

--- civicrm.js
+++ civicrm.js
@@ -10,58 +10,68 @@ var config = {
   api_key:'the user key'
 };
 */
-var crmAPI = function () {
-  this.options ={};
-};
-
-
-crmAPI.init = function (options) {
+var crmAPI = function civicrm (options) {
   this.options = options;
 };
 
-crmAPI.urlize = function (entity,action) {
+p = crmAPI.prototype;
+
+p.urlize = function (entity,action) {
   return this.options.server +this.options.path+ '?' + qs.stringify ({ 
     entity: entity,
     action: action,
   });
 }
 
-crmAPI.call = function (entity,action,params,callback) {
-  var post = params;
+p.call = function (entity,action,params,callback) {
+  var post = {};
+  if (typeof(params) === "object") {
+    post = params;
+  } else {
+    post = { id: params }
+  }
   post.sequential= 1;
   post.json = 1;
   post.key = this.options.key;
   post.api_key = this.options.api_key;
+  var uri =  this.urlize(entity,action);
   request({
     uri:this.urlize(entity,action),method:'POST',
     form: post}
     ,function(error, response, body){
        if (!error && response.statusCode == 200) {
-        callback (JSON.parse(body));
+         try {
+           callback (JSON.parse(body));
+         } catch (e) {
+           callback ({is_error:false,error_message:body})
+         }
        } else {
-         callback ({is_error:1, error_message: 'invalid url '+response.request.uri.href});
+         console.log(uri);
+         callback ({is_error:1, error_message: 'invalid url '+uri});
        }
   });
 };
 
-crmAPI.get = function (entity, params,callback) {
+//TODO add an error first param ? + option to throw it
+//
+p.get = function (entity, params,callback) {
   this.call (entity,'get',params,callback);
 }
 
-crmAPI.getSingle = function (entity, params,callback) {
+p.getSingle = function (entity, params,callback) {
   this.call (entity,'getsingle',params,callback);
 }
 
-crmAPI.create = function (entity, params,callback) {
+p.create = function (entity, params,callback) {
   this.call (entity,'create',params,callback);
 }
 
-crmAPI.update = function (entity, params,callback) {
+p.update = function (entity, params,callback) {
   // todo, test if params.id is set
   this.call (entity,'create',params,callback);
 }
 
-crmAPI.delete = function (entity, params,callback) {
+p.delete = function (entity, params,callback) {
   this.call (entity,'delete',params,callback);
 }
 
